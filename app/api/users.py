102d80c07a9e8a9799e8a5ba93359cafb2684a96
Izeosi <izeosidanesiobemeata@gmail.com>
2026-09-04 17:01:49 +0100

    Modifying a User

--- app/api/users.py
+++ app/api/users.py
@@ -49,7 +49,16 @@ def create_user():
     response.headers['Location'] = url_for('api.get_user', id=user.id)
     return response
 
-
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
-    pass
+    user = User.query.get_or_404(id)
+    data = request.get_json() or {}
+    if 'username' in data and data['username'] != user.username \
+            and User.query.filter_by(username=data['username']).first():
+        return bad_request('Please use a different name.')
+    if 'email' in data and data['email'] != user.email \
+            and User.query.filter_by(email=data['email']).first():
+        return bad_request('Please use a different email address.')
+    user.from_dict(data, new_user=False)
+    db.session.commit()
+    return jsonify(user.to_dict())
