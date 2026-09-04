39f7a41f3516ecf397a706fc1016d20b46cee871
Izeosi <izeosidanesiobemeata@gmail.com>
2026-09-04 17:32:15 +0100

    Support for User Tokens

--- app/model.py
+++ app/model.py
@@ -1,5 +1,8 @@
 import json
-from datetime import datetime, UTC
+import base64
+from datetime import datetime, UTC, timedelta
+import os
+from enum import unique
 
 from time import time
 
@@ -92,6 +95,8 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=lambda: datetime.now(UTC))
     last_messages_read_time = db.Column(db.DateTime)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     followed = db.relationship(
@@ -205,6 +210,25 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                 setattr(self, field, data[field])
             if new_user and 'password' in data:
                 self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.now(UTC)
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.now(UTC) - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.now(UTC):
+            return None
+        return user
 
 
 class Post(SearchableMixin, db.Model):
